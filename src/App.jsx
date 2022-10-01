79fd0e19c6683b7280a9bd6ba2db8a9a5bebe6de
FredySanchezLopez <102775554+FredySanchezLopez@users.noreply.github.com>
2022-09-30 22:38:48 -0500

    Segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,22 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 import './App.css'
 import CardResident from './components/CardResident'
+import FilterList from './components/FilterList'
 import LocationInfo from './components/LocationInfo'
+import ErrorScreen from './components/ErrorScreen'
 import getRandomNumber from './utils/getRandomNumber'
 
 function App() {
 
   //Hacer la petición a la API USESTATE, USEEFFECT Y AXIOS
-
+  //Para guardar la informacion de la location de la API
   const [location, setLocation] = useState()
+  // Para guardar la informacion del INput
   const [searchInput, setSearchInput] = useState('')
-  
-  console.log(searchInput);
+  // Para guardar las sugerencias de la API
+  const [suggestedList, setSuggestedList] = useState()
+  // Para indicar si es que hay un error
+  const [hasError, setHasError] = useState(false)
 
   useEffect(() => {
     let id = getRandomNumber()
@@ -25,38 +30,72 @@ function App() {
     const URL = `https://rickandmortyapi.com/api/location/${id}`
 
     axios.get(URL)
-      .then(res => setLocation(res.data))
-      .catch(err => console.log(err))
+      .then(res => {
+        setHasError(false)
+        setLocation(res.data)
+        })
+      .catch(err => setHasError(true))
   }, [searchInput])
 
   const handleSubmit = e => {
     e.preventDefault()
     setSearchInput(e.target.idLocation.value)
   }
+
+  const handleChange = e => {
+
+    if(e.target.value === '') {
+      return setSuggestedList()
+    }
+    else{
+      const URL = `https://rickandmortyapi.com/api/location?name=${event.target.value}`
+
+      axios.get(URL)
+        .then(res => setSuggestedList(res.data.results))
+        .catch(err => console.log(err))
+    }
+  }
   
+  
+
+
   return (
     <div className="App">
       <h1>Rick And Morty</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          id='idLocation'
-          placeholder='Enter another number from 1 to 126' type="text" 
-        />
-        <button>Search</button>
-      </form>
-      <LocationInfo location={location} />
-      <div>
-        {
-          location?.residents.map(url => (
-            <CardResident
-              key={url}
-              url={url}
-            />
-          ))
-        }
-      </div>
-    </div>
-
+        <form onSubmit={handleSubmit}>
+          <input
+            id='idLocation'
+            placeholder='Enter another number from 1 to 126' type="text" 
+            onChange={handleChange}
+          />
+          <button>Search</button>
+          <FilterList 
+            suggestedList = {suggestedList}
+            setSearchInput = {setSearchInput}
+          />
+        </form>
+      
+      {
+        hasError?
+          <ErrorScreen/>
+        :
+        <>
+          <div>
+            <LocationInfo location={location} />
+              <div>
+                {
+                  location?.residents.map(url => (
+                    <CardResident
+                      key={url}
+                      url={url}
+                    />
+                  ))
+                }
+              </div>
+            </div>
+        </>   
+      }
+    </div>   
   )
 }
 
